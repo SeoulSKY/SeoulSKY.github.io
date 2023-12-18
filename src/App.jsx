bc6e714ff5b437e9b60ae8884f43acb47146e1fb
SeoulSKY <email@seoulsky.org>
2023-12-18 14:15:57 -0600

    Change cursor style

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
+import AnimatedCursor from "react-animated-cursor"
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Skills, Projects, StarsCanvas } from "./components";
 
 const App = () => {
   return (
     <BrowserRouter>
-
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
@@ -21,6 +21,17 @@ const App = () => {
           <Contact />
         </div>
       </div>
+
+      <AnimatedCursor
+        color="256, 256, 256"
+        outerSize={35}
+        innerScale={1}
+        outerScale={1.7}
+        outerAlpha={0}
+        outerStyle={{
+          border: '3px solid #fff'
+        }}
+      />
     </BrowserRouter>
   );
 }
